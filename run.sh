a9ddffa201ba7f9cdc34de249e875e394db394cd
danny <asdc.sinica@gmail.com>
2017-11-01 11:20:13 +0800

    add run charset

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 charset=$(env|grep charset|cut -d"=" -f 2)
 
 if [ ! -z "$charset" ] ; then
-	sed -i "s/charset UTF-8/charset $charset/g" /etc/nginx.conf
+	sed -i "s/charset UTF-8/charset $charset/g" /etc/nginx/nginx.conf
 fi
 
 service nginx start
